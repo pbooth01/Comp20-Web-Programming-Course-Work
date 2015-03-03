0b14f2892514a630e18a5eab622ffff5576ada8b
Phillip Booth <phillip.booth2015@gmail.com>
2015-03-03 18:50:23 -0500

    final lab

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,14 @@
-// Replace this with your code...
+function init(){
+  var img = new Image();
+  img.src = 'pacman10-hp-sprite.png';
+
+  var canvas = document.getElementById('game_canvas');
+    if (canvas.getContext){
+      var ctx = canvas.getContext('2d');
+
+      img.onload = function(){
+      ctx.drawImage(img, 317, 0, 470, 138, 0, 0, 470, 138);
+      ctx.drawImage(img, 78, 21, 17, 17, 38, 30, 15, 15);
+      };
+   } 
+}
